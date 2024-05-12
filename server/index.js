7dc478427f08218a00c830b70ea20bb1a129252f
masterthuku <peterthuku706@gmail.com>
2024-05-12 18:17:00 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 console.log('Hello from server')
+
+const express = require('express');
+const PORT = 3000;
+
+const app = express();
+
+//creating an api
+app.get('/hello world', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT,"0.0.0.0", () => {
+    console.log(`Server listening on port ${PORT}`)
+});
