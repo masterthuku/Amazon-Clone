6b5e60fc27e75acad8e3589e1e0cc05b0c5fc35a
masterthuku <peterthuku706@gmail.com>
2024-05-13 02:40:18 +0300

    update

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
-console.log('Hello from server')
+//imports from packages
+const express = require("express");
+const mongoose = require("mongoose");
 
-const express = require('express');
+//initialization
 const PORT = 3000;
-
 const app = express();
+const DB = "mongodb+srv://peterthuku706:SgGre3aSHHgUPHpz@cluster0.vwpzrvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-//creating an api
-app.get('/hello world', (req, res) => {
-    res.send('Hello World')
-})
+//imports from files
+const authRouter = require("./routes/auth");
 
-app.listen(PORT,"0.0.0.0", () => {
-    console.log(`Server listening on port ${PORT}`)
+//middleware
+app.use(authRouter);
+
+// connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on port ${PORT}`);
 });
